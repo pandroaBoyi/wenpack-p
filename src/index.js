6bb990a49482aae6d0e2d1a5c2ebc60d62bd140b
codingsherlock <664516715@qq.com>
2019-06-24 17:22:00 +0800

    Production

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
-import _ from 'lodash';
-import printMe from './print';
-import './style.css';
+import { cube } from './math.js';
+
 
 function component () {
-    const element = document.createElement('div')
-    const btn = document.createElement('button')
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe
-
-    element.appendChild(btn)
+    var element = document.createElement('div')
+    element.innerHTML = [
+        'Hello Webpack',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
     return element
     
 }
 let element = component();
 document.body.appendChild(element);
-
-if (module.hot) {
-    module.hot.accept('./print.js', function() {
-        console.log('Accepting the updated printMe module!')
-        // printMe()
-        document.body.removeChild(element);
-        element = component();
-        document.body.appendChild(element);
-    })
-}
