6bba8959bb4af5a42f45a209cf4f7e5f4749356d
codingsherlock <664516715@qq.com>
2019-06-24 16:44:02 +0800

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print';
+import './style.css';
 
 function component () {
     const element = document.createElement('div')
@@ -14,5 +15,15 @@ function component () {
     return element
     
 }
+let element = component();
+document.body.appendChild(element);
 
-document.body.appendChild(component())
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!')
+        // printMe()
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    })
+}
