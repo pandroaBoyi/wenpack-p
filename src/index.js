2fe55a7472b1881a60c07906cd5ce4d6cff38a65
codingsherlock <664516715@qq.com>
2019-06-25 13:34:32 +0800

    Caching

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-// import _ from 'lodash';
+import _ from 'lodash';
+// import Print from './print';
 
 
-function getComponent () {
-    // var element = document.createElement('div')
+function component () {
+    var element = document.createElement('div')
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    // element.onclick = Print.bind(null, 'Hello webpack!')
 
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then( _ => {
-        var element = document.createElement('div');
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-        return element;
-    }).catch(error => 'An error occurred while loading the component')
+    return element;
     
 }
-// let element = component();
-// document.body.appendChild(element);
-getComponent().then(component => {
-    document.body.appendChild(component)
-})
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
     entry: {
         index: './src/index.js'
     },
+    plugins:[
+        new HtmlWebpackPlugin ({
+            title: 'Caching'
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.HashedModuleIdsPlugin()
+    ],
     output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
+        filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
     }
 };
